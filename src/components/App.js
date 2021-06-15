4777bbc5bbe9b30e8c7fa3e35557fe4873b0dd2a
Piotr Gaik <piotgie@gmail.com>
2021-06-15 08:52:55 +0200

    init

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import TaskList from '../components/TaskList'
+import '../App.css'
+import TaskListContextProvider from '../context/TaskListContext'
+import TaskForm from './TaskForm'
+import Header from './Header'
+
+function App() {
+    return (
+            <TaskListContextProvider>
+
+            <div className="container">
+                <div className="app-wrapper">
+                    <Header />
+                    <div className="main">
+                        <TaskForm />
+                        <TaskList />
+                    </div>
+                </div>
+            </div>
+                
+            </TaskListContextProvider>
+    )
+}
+
+export default App
---
+++ src/components/TaskForm.js
@@ -0,0 +1,59 @@
+import React, {useContext, useState, useEffect} from 'react'
+import TaskListContextProvider, {TaskListContext} from '../context/TaskListContext'
+import TaskList from './TaskList'
+
+const TaskForm = () => {
+
+    const {addTask, clearList, editItem, editTask} = useContext(TaskListContext)
+
+    const [title, setTitle] = useState("")
+
+    const handleChange = e => {
+        setTitle(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if(!editItem) {
+            addTask(title)
+            setTitle('')
+        }else {
+            editTask(title, editItem.id)
+        }
+    }
+
+    useEffect(()=> {
+        if (editItem) {
+            setTitle(editItem.title)
+        } else {
+            setTitle('')
+        }
+    }, [editItem])
+
+    return (
+        <form onSubmit={handleSubmit} className="form">
+            <input 
+            onChange={handleChange}
+            value={title}
+            type="text" 
+            className="task-input"
+            placeholder="Add Task"
+            required
+            />
+            <div className="buttons">
+                <button 
+                className="btn add-task-btn" 
+                type="submit">
+                    {editItem ? 'Edit Task' : 'Add Task'}
+                </button>
+                <button 
+                onClick={clearList}
+                className="btn clear-btn">
+                    Clear
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default TaskForm
